1c5c705abecb0650d942c23b335bdd6f1760ddc8
fgarciamatar <fgarciamatar@gmail.com>
2025-06-27 00:26:52 -0300

    cambios hook

--- components/Chofer/SelectChofer.tsx
+++ components/Chofer/SelectChofer.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { Text, View } from 'react-native';
-import { useSendLocation } from '../../hooks/choferHook';
+import { useSendLocation } from "../../hooks/choferHook";
 
 export default function HomeScreen() {
   // busId (por ahora hardcodeado)
+const [selectChofer, setSelectChofer] = useState(0)
+
   const busId = 'bus-123';
   useSendLocation(busId);
+
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
